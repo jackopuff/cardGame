bbcd2acc71520269a68b74859f06b5c0e8345281
jackopuff <jcar8281@740s-pl-128.local>
2016-10-25 13:27:14 -0600

    Added getterMethods for the Card class

--- cardGame/Model/Card.swift
+++ cardGame/Model/Card.swift
@@ -16,6 +16,20 @@ class Card
     init()
     {
         backImage = UIImage()
-        isFaceUP = false
+        isFaceUp = false
+    }
+    func getBackImage() ->UIImage
+    {
+        return backImage
+    }
+    func isFacing() ->Bool
+    {
+        return isFaceUp
+    }
+    func toString() ->String
+    {
+        let description = "This card is \(isFaceUp)"
+        
+        return description
     }
 }
