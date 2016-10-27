e317f523063b45e0d1f120e310d16c3411103366
jackopuff <jcar8281@740s-pl-128.local>
2016-10-27 11:57:30 -0600

    added properties to the card data members

--- cardGame/Model/Card.swift
+++ cardGame/Model/Card.swift
@@ -11,7 +11,27 @@ import UIKit
 class Card
 {
     private var backImage : UIImage
+    {
+        get
+        {
+            return self.backImage
+        }
+        set(backImage)
+        {
+            self.backImage = backImage
+        }
+    }
     private var isFaceUp : Bool
+    {
+        get
+        {
+            return self.isFaceUp
+        }
+        set(isFaceUp)
+        {
+            self.isFaceUp = isFaceUp
+        }
+    }
     
     init()
     {
@@ -26,9 +46,20 @@ class Card
     {
         return isFaceUp
     }
+    
+    func setFacing(isFaceUp : Bool)
+    {
+        self.isFaceUp = isFaceUp
+    }
+    
+    func setBackImage(backImage : UIImage)
+    {
+        self.backImage = backImage
+    }
+    
     func toString() ->String
     {
-        let description = "This card is \(isFaceUp)"
+        let description = "This card is \(isFaceUp) and \(self.getBackImage()) is the image."
         
         return description
     }
