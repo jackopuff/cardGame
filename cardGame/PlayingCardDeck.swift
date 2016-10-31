5940ccd50d569352e4d2b1adce3936adc09899d8
jackopuff <jcar8281@740s-pl-128.local>
2016-10-31 12:43:06 -0600

    Added shuffle and order functions

--- cardGame/PlayingCardDeck.swift
+++ cardGame/PlayingCardDeck.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+
+class PlayingCardDeck : Deck
+{
+    internal lazy var cards = [PlayingCard]()
+    override init()
+    {
+        super.init()
+    }
+    func shuffleDeck()-> Void
+    {
+        
+    }
+    func orderDeck() -> Void
+    {
+        
+    }
+}
