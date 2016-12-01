33aa0b0fb31727896ca15e12fb4a7725b072ff16
jackopuff <jcar8281@740s-pl-128.local>
2016-12-01 13:53:35 -0700

    began game code

--- cardGame/StupidGame.swift
+++ cardGame/StupidGame.swift
@@ -9,14 +9,58 @@
 import Foundation
 class StupidGame
 {
-    internal var GameDeck: PlayingCardDeck
-    internal lazy var MatchDeck = [Card]()
+    internal var GameDeck : PlayingCardDeck
+    internal var hand : [PlayingCard]
+    internal var score : Int
+
+    //constructor
+    init()
     {
         GameDeck = PlayingCardDeck()
-        MatchDeck = [Card]()
+        hand = [PlayingCard]()
+        self.score = 0
     }
-    
+    //
     func startGame()->Void
     {
+        GameDeck.shuffleDeck()
+        drawCards()
     }
+    private func drawCards() -> Void
+    {
+        hand.append((GameDeck.drawCard() as? PlayingCard)!)
+        hand.append((GameDeck.drawCard() as? PlayingCard)!)
+        
+    }
+    func checkMatch() -> Bool
+    {
+    let hasMatch :Bool
+    if(hand[0].rank == hand[1].rank) || (hand[0].suit == hand[1].suit)
+    {
+        hasMatch = true
+    }
+    else
+    {
+        hasMatch = false
+    }
+    return hasMatch
+    }
+    func playMatchGame() -> Void
+    {
+        if GameDeck.cards.count<0
+        {
+            if checkMatch()
+            {
+                score += 5
+            }
+            else
+            {
+                score = score-2
+            }
+           
+        }
+        hand.removeAll()
+        drawCards()
+    }
+
 }
